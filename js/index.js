2c7055f28ff340fc57fe47cd881924f1bbe7daf2
chazkiker2 <chazkiker@gmail.com>
2020-09-29 15:37:44 -0400

    add pop-up window for sign-up div.btns

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 // Nav Link Events
 const navLinks = document.querySelectorAll("a.nav-link");
 navLinks.forEach(a => {
-  a.addEventListener("mouseover", (e) => {
+  a.addEventListener("mouseover", () => {
     a.style.backgroundColor = "black";
     a.style.color = "white";
   });
-  a.addEventListener("mouseout", e => {
+  a.addEventListener("mouseout", () => {
     a.style.backgroundColor = "transparent";
     a.style.color = "#212529";
   });
@@ -14,3 +14,12 @@ navLinks.forEach(a => {
   });
 });
 
+const divBtns = document.querySelectorAll("div.btn");
+console.log(divBtns);
+let params = `scrollbars=no, resizable=no, status=no, location=no, toolbar=no, menubar=no, width=550, height=580, left=100, top=100`;
+divBtns.forEach( btn => {
+  btn.addEventListener("click", () => {
+    window.open("https://www.facebook.com/login", "Facebook Login", params);
+    window.focus();
+  });
+});
