59c006689a6a98a3439c182034141355236c8714
chazkiker2 <chazkiker@gmail.com>
2020-09-29 15:12:31 -0400

    add nav link event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
-// Your code goes here
+// Nav Link Events
+const navLinks = document.querySelectorAll("a.nav-link");
+navLinks.forEach(a => {
+  a.addEventListener("mouseover", (e) => {
+    a.style.backgroundColor = "black";
+    a.style.color = "white";
+  });
+  a.addEventListener("mouseout", e => {
+    a.style.backgroundColor = "transparent";
+    a.style.color = "#212529";
+  });
+  a.addEventListener("click", e => {
+    e.preventDefault();
+  });
+});
+
